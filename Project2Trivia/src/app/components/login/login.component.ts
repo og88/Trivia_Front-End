14d0d803f93990a261e336380c743e51d73f4d7c
Omar Garcia <og65edu@gmail.com>
2019-02-19 21:13:53 -0500

    Added login functionality

--- Project2Trivia/src/app/components/login/login.component.ts
+++ Project2Trivia/src/app/components/login/login.component.ts
@@ -1,23 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from 'src/app/models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+@Injectable()
 export class LoginComponent implements OnInit {
 
   title: string = "Trivia Hero!";
   logo: string = "http://placehold.it/250x250?text=Trivia Hero!";
   description: string = "A Trivia Game";
+  loginObj = {
+    username : '',
+    password : ''
+  }
 
-  constructor() { }
+  constructor(private http: HttpClient, private router : Router) { }
 
   ngOnInit() {
   }
+configUrl = 'http://ec2-3-17-244-111.us-east-2.compute.amazonaws.com:8080/project2/rest/login'
+//configUrl = 'http://localhost:8080/project2/rest/login'
 
-  onLogin(){
-    
+
+onLogin(){
+
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Access-Control-Allow-Origin': 'http://localhost:8080',
+						'Access-Control-Allow-Credentials': 'true',
+						'Access-Control-Allow-Methods': 'POST'
+    })
+  };
+
+  const newLocal = this;
+
+    console.log('User name ' + newLocal.loginObj.username + ' password ' + newLocal.loginObj.password );
+    if(newLocal.loginObj.username != ''){
+      if(newLocal.loginObj.password !=''){
+        console.log('works');
+        this.http.post<User>(this.configUrl, newLocal.loginObj, httpOptions)
+        .subscribe(Response => {
+          if(Response.username == this.loginObj.username){
+            localStorage.setItem('currentUser', JSON.stringify(Response));
+            this.router.navigate(["/main-menu"])
+          }
+        });
+      } else {
+        console.log('failed');
+      }
+    } else {
+      console.log('failed');
+    }
   }
-
 }
